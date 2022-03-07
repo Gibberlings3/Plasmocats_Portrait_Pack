ea89ef595c9dfc41fc7a5634768a9704d915e396
CamDawg <pcamagna@yahoo.com>
2022-03-07 14:02:22 -0800

    v4 packaging, PI hooks

--- package_mod.bat
+++ package_mod.bat
@@ -5,8 +5,8 @@ REM /* this initializes the values for the different variables referenced in com
 call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
-set "mod_name=Plasmocat’s Portrait Pack"
-set mod_version=v3
+set "mod_name=Plasmocat's Portrait Pack"
+set mod_version=v4
 set mod_folder=plasmoportraits
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
